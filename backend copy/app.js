c0b4c799643964c6d6dd1ff64be7a280df5b3ed0
ZoetorZ <43775310+ZoetorZ@users.noreply.github.com>
2024-04-06 17:48:12 +0200

    Add files via upload
    initial commit

---
+++ backend copy/app.js
@@ -0,0 +1,67 @@
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+
+var app = express();
+
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get("env") === "development" ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render("error");
+});
+
+module.exports = app;
+
+require("dotenv").config();
+
+var mysql = require("mysql");
+var connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+});
+
+connection.connect();
+
+connection.query("SELECT 1", function (error, results, fields) {
+    if (error) throw error;
+    // connected!
+});
+
+const { select } = require("@nodejscart/mysql-query-builder");
+
+async function test() {
+    const expenses = await select("*").from("expenses").execute(connection);
+    console.log(expenses);
+}
+
+test();
